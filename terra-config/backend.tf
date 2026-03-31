67526cc35678cb653ca152392ff394614f4917cc
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-03-31 17:53:04 +0530

    Delete terra-config/backend.tf

--- terra-config/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bijesh-s3-buckett"  # Change if the name already exists. 
-    key            = "eks/terraform.tfstate"       
-    region         = "us-east-1"                   
-    encrypt        = true
-  }
-}
