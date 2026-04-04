90755fc3cf7c68b35cd65467ccadfa14e826ad17
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-05 00:13:58 +0530

    Update node.tf
    Adding a node group for worker node

--- terraform/module/eks/node.tf
+++ terraform/module/eks/node.tf
@@ -1,1 +1,14 @@
 
+# Node group
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "worker-nodes"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+}
