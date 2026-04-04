fc9f180ff88954b18ad18a50448b62a14557b3dd
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-05 00:59:44 +0530

    Update main.tf
    Defined subnet addresses with region (ax)

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,9 @@ module "vpc" {
   source = "./modules/vpc"
 
   cidr_block = "10.0.0.0/16"
+  public_subnets  = ["10.0.1.0/24"]
+  private_subnets = ["10.0.3.0/24"]
+  azs             = ["us-east-1"]
 }
 
 module "eks" {
