d3ec01ef4772a83775be43c525675647b8737338
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-04 21:01:51 +0530

    Update main.tf
    Adding terraform infra modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,18 @@
 module "vpc" {
   source = "./modules/vpc"
+
+  cidr_block = "10.0.0.0/16"
 }
 
 module "eks" {
-  source     = "./modules/eks"
+  source = "./modules/eks"
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+}
+
+module "rds" {
+  source = "./modules/rds"
+
+  vpc_id = module.vpc.vpc_id
 }
