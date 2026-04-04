6dd67432a2b9bbc4faaad292781e18fea36d2603
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-05 00:21:52 +0530

    Update node.tf
    Adding worker nodes + Node IAM role with IAM policy

--- terraform/module/eks/node.tf
+++ terraform/module/eks/node.tf
@@ -1,5 +1,6 @@
 
-# Node group
+# Node group for worker nodes
+
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "worker-nodes"
@@ -12,3 +13,39 @@ resource "aws_eks_node_group" "nodes" {
     min_size     = 1
   }
 }
+
+##  Adding node IAM roles 
+
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+## Attaching IAM Policy to worker node
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "registry_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
