68d0142286871668dea1b64c547c3cb64c22ba6c
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-07 13:25:45 +0530

    Update frontend.tf
    Adding k8s deployment & service file for frontend

--- terraform/k8s/frontend.tf
+++ terraform/k8s/frontend.tf
@@ -1,1 +1,55 @@
+resource "kubernetes_deployment" "frontend" {
+  metadata {
+    name      = "frontend"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
 
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "frontend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "frontend"
+        }
+      }
+
+      spec {
+        container {
+          name  = "frontend"
+          image = var.frontend_image
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "frontend" {
+  metadata {
+    name      = "frontend-service"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "frontend"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
