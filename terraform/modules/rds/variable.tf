d8ce632f28857127c3d9e1ce44e4df05bd408fb3
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-07 19:43:20 +0530

    Update variable.tf
    RDS variable modified as strings

--- terraform/modules/rds/variable.tf
+++ terraform/modules/rds/variable.tf
@@ -1,4 +1,9 @@
-variable "db_username" {}
-variable "db_password" {}
+variable "db_username" {
+type = string
+}
+variable "db_password" {
+type = string
+}
+
 variable "subnet_ids" {}
 variable "vpc_id" {}
