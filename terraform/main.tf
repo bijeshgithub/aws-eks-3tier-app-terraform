fe13a49b14b77030d8b652a84dd59a3ad45721f4
bijeshgithub <kashyapbijesh94@gmail.com>
2026-04-07 19:13:14 +0530

    adding vpc_id

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,15 @@ module "vpc" {
 
 module "eks" {
   source = "./modules/eks"
-
+  
+  vpc_id  = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 }
 
 module "rds" {
   source = "./modules/rds"
   
+  vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
 }
