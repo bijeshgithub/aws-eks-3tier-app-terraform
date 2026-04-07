fd10a2b44a60c4ddd1ed34f724cb712532ab2ae5
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-07 15:08:58 +0530

    Update main.tf
    Modifying gateways

--- terraform/module/vpc/main.tf
+++ terraform/module/vpc/main.tf
@@ -6,21 +6,39 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Public Subnets
+# -----------------------------
+#  Internet Gateway
+# -----------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+# -----------------------------
+# Public Subnets (ALB)
+# -----------------------------
 resource "aws_subnet" "public" {
   count = length(var.public_subnets)
 
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnets[count.index]
-  map_public_ip_on_launch = true
   availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-${count.index}"
+
+    "kubernetes.io/cluster/my-eks" = "shared"
+    "kubernetes.io/role/elb"       = "1"
   }
 }
 
-# Private Subnets
+# -----------------------------
+# Private Subnets (EKS Nodes)
+# -----------------------------
 resource "aws_subnet" "private" {
   count = length(var.private_subnets)
 
@@ -28,11 +46,79 @@ resource "aws_subnet" "private" {
   cidr_block        = var.private_subnets[count.index]
   availability_zone = var.azs[count.index]
 
-tags = {
-  Name = "private-${count.index}"
+  tags = {
+    Name = "private-${count.index}"
 
-  "kubernetes.io/cluster/my-eks"    = "shared"
-  "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/my-eks"    = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
   }
-  
+}
+
+# -----------------------------
+#  Elastic IP for NAT
+# -----------------------------
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+# -----------------------------
+# NAT Gateway (in public subnet)
+# -----------------------------
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "eks-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# -----------------------------
+# Public Route Table
+# -----------------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Associate Public Subnets
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# -----------------------------
+# Private Route Table
+# -----------------------------
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+# Associate Private Subnets
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
