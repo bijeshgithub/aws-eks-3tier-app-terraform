e9de01bb87e67a748b68f8712e75f494c21fddfd
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-07 21:36:42 +0530

    Create alb.tf
    Adding alb controller

---
+++ terraform/alb.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  depends_on = [module.eks]
+}
