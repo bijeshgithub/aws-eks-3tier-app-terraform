dddc376b9ac4c7c12de403c70719a850dbcf3299
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-07 23:30:02 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,6 +18,35 @@ terraform {
 
     }
   }
+
+# EKS Data
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+# Kubernetes Provider
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(
+    data.aws_eks_cluster.cluster.certificate_authority[0].data
+  )
+  token = data.aws_eks_cluster_auth.cluster.token
+}
+
+# Helm Provider
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.cluster.certificate_authority[0].data
+    )
+    token = data.aws_eks_cluster_auth.cluster.token
+  }
+}
 }
 # Configure the AWS Provider
 provider "aws" {
