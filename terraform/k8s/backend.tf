ed9e689c981ff0071917dd79743dabc37b92cf76
bijeshgithub <73757655+bijeshgithub@users.noreply.github.com>
2026-04-07 13:28:31 +0530

    Update backend.tf
    Modified k8s deployment & service file - backend

--- terraform/k8s/backend.tf
+++ terraform/k8s/backend.tf
@@ -1,1 +1,60 @@
+resource "kubernetes_deployment" "backend" {
+  metadata {
+    name      = "backend"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
 
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "backend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "backend"
+        }
+      }
+
+      spec {
+        container {
+          name  = "backend"
+          image = var.backend_image
+
+          port {
+            container_port = 5000
+          }
+
+          env {
+            name  = "DB_HOST"
+            value = var.db_endpoint
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "backend" {
+  metadata {
+    name      = "backend-service"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "backend"
+    }
+
+    port {
+      port        = 5000
+      target_port = 5000
+    }
+
+    type = "ClusterIP"
+  }
+}
